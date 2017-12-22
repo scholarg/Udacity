de58bf1972bbb89dfe89baf5b8c44c3fe1f6f4b2
yisuguan <yisuguan@gmail.com>
2017-12-22 17:01:30 +0800

    study Set

--- frontend_study/JavaScriptStudy/WeakSet.js
+++ frontend_study/JavaScriptStudy/WeakSet.js
@@ -11,6 +11,7 @@ const uniqueFlavors = new WeakSet()
 const flavor1 = { flavor: 'chocolate' }
 const flavor2 = {flavor: 'orange'}
 
-uniqueFlavors.add(flavor1, flavor2)
+uniqueFlavors.add(flavor1)
+uniqueFlavors.add(flavor2)
 
 uniqueFlavors.add(flavor1)
