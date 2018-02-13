aa2b3e885523f07ac56cc25a082a37a72e8d2073
yisuguan <yisuguan@gmail.com>
2018-02-13 22:12:58 +0800

    change

---
+++ frontend_study/street_map/js/localtions.js
@@ -0,0 +1,41 @@
+var locations = [{
+        title: '夫子庙',
+        pinyin: 'FuZiMiao',
+        position: {
+            lat: 32.021719,
+            lng: 118.79187
+        }
+    },
+    {
+        title: '大报恩寺',
+        pinyin: 'DabaoEnSi',
+        position: {
+            lat: 32.009386,
+            lng: 118.783343
+        }
+    },
+    {
+        title: '莫愁湖公园',
+        pinyin: 'MoChou Park',
+        position: {
+            lat: 32.036153,
+            lng: 118.762798
+        }
+    },
+    {
+        title: '雨花台',
+        pinyin: 'YuHuaTai',
+        position: {
+            lat: 31.995501,
+            lng: 118.78247
+        }
+    },
+    {
+        title: '钟山风景区',
+        pinyin: 'ZhongShan',
+        position: {
+            lat: 32.072822,
+            lng: 118.853855
+        }
+    }
+]
--- frontend_study/street_map/js/map.js
+++ frontend_study/street_map/js/map.js
@@ -3,47 +3,6 @@ var markers = [];
 /**
  * 包含地点名称和坐标的对象数组
  */
-var locations = [{
-        title: '夫子庙',
-        pinyin: 'FuZiMiao',
-        position: {
-            lat: 32.021719,
-            lng: 118.79187
-        }
-    },
-    {
-        title: '大报恩寺',
-        pinyin: 'DabaoEnSi',
-        position: {
-            lat: 32.009386,
-            lng: 118.783343
-        }
-    },
-    {
-        title: '莫愁湖公园',
-        pinyin: 'MoChou Park',
-        position: {
-            lat: 32.036153,
-            lng: 118.762798
-        }
-    },
-    {
-        title: '雨花台',
-        pinyin: 'YuHuaTai',
-        position: {
-            lat: 31.995501,
-            lng: 118.78247
-        }
-    },
-    {
-        title: '钟山风景区',
-        pinyin: 'ZhongShan',
-        position: {
-            lat: 32.072822,
-            lng: 118.853855
-        }
-    }
-]
 
 var styles = [
   {
