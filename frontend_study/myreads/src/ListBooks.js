f3f1518ab47be98718e25cfc54c063ff3b1bad5c
yisuguan <yisuguan@gmail.com>
2018-02-22 10:09:55 +0800

    1

--- frontend_study/myreads/src/ListBooks.js
+++ frontend_study/myreads/src/ListBooks.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Books from './Books';
 
 class ListBooks extends React.Component {
-    // eslint-disable-next-line
     static propTypes = {
         booksData: PropTypes.array.isRequired,
         updateBook: PropTypes.func.isRequired
